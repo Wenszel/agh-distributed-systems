8ec59dbff348505003133441779f3a088bcd70a3
Wenszel <wiktor.smaga13@gmail.com>
2025-03-16 14:11:02 +0100

    Added UDP support for client

--- LAB01/src/main/java/org/example/Client.java
+++ LAB01/src/main/java/org/example/Client.java
@@ -1,47 +1,64 @@
 package org.example;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.Scanner;
 
 import static org.example.Server.SERVER_HOSTNAME;
 import static org.example.Server.SERVER_PORT;
 
 public class Client {
-
-    public static void main(String[] args) {
+    private static SocketState state = SocketState.TCP;
+    private enum SocketState {
+        UDP, TCP
+    }
+    public static void main(String[] args) throws SocketException {
         Scanner scanner = new Scanner(System.in);
+        DatagramSocket datagramSocket = new DatagramSocket();
         try (Socket serverSocket = new Socket(SERVER_HOSTNAME, SERVER_PORT)) {
-            SocketHandler socketHandler = new SocketHandler(serverSocket);
-            setUsername(scanner, socketHandler);
-            listenToNewMessagesFromServer(socketHandler);
-            listenToUserNewMessages(scanner, socketHandler);
+            TCPSocketHandler TCPSocketHandler = new TCPSocketHandler(serverSocket);
+            setUsername(scanner, TCPSocketHandler);
+            listenToNewMessagesFromServer(TCPSocketHandler);
+            listenToUserNewMessages(scanner, TCPSocketHandler, datagramSocket);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static void listenToUserNewMessages(Scanner scanner, SocketHandler socketHandler) throws IOException {
+    private static void listenToUserNewMessages(Scanner scanner, TCPSocketHandler TCPSocketHandler, DatagramSocket datagramSocket) throws IOException {
         while (true) {
             String message = scanner.nextLine();
-            socketHandler.send(message);
+            if (message.equals("U")) {
+                System.out.println("Switched to UDP communication");
+                state = SocketState.UDP;
+            }
+            if (state.equals(SocketState.TCP)) TCPSocketHandler.send(message);
+            else {
+                byte[] buff = message.getBytes();
+                DatagramPacket dp = new DatagramPacket(buff, buff.length);
+                datagramSocket.send(dp);
+                state = SocketState.TCP;
+            }
             if (message.equals(":wq")) break;
         }
     }
 
-    private static void listenToNewMessagesFromServer(SocketHandler socketHandler) {
+    private static void listenToNewMessagesFromServer(TCPSocketHandler TCPSocketHandler) {
         new Thread(() -> {
             try {
-                while(true) System.out.println(socketHandler.receive());
+                while(true) System.out.println(TCPSocketHandler.receive());
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }).start();
     }
 
-    private static void setUsername(Scanner scanner, SocketHandler socketHandler) throws IOException {
+    private static void setUsername(Scanner scanner, TCPSocketHandler TCPSocketHandler) throws IOException {
         System.out.print("Enter your username: ");
         String username = scanner.nextLine();
-        socketHandler.send(username);
+        TCPSocketHandler.send(username);
     }
 }

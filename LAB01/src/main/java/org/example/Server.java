2ab70be312d6cbc1b2335bf37c00d6e8f5285dc4
Wenszel <wiktor.smaga13@gmail.com>
2025-03-16 14:23:36 +0100

    feat: moved tcp and udp server to different threads

--- LAB01/src/main/java/org/example/Server.java
+++ LAB01/src/main/java/org/example/Server.java
@@ -1,38 +1,53 @@
 package org.example;
 
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
 import java.util.*;
 
 public class Server {
     public static final int SERVER_PORT = 8080;
     public static final String SERVER_HOSTNAME = "localhost";
-    public static final Set<SocketHandler> clientsSockets = new HashSet<>();
-    public static final HashMap<SocketHandler, String> clients = new HashMap<>();
+    public static final HashMap<TCPSocketHandler, String> clients = new HashMap<>();
+    public static final Map<TCPSocketHandler, Thread> clientsThreads = new HashMap<>();
 
-    public static final Map<SocketHandler, Thread> clientsThreads = new HashMap<>();
 
     public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
-            System.out.println("TCP server is listening on port: " + SERVER_PORT);
-            while (true) {
-                Socket clientSocket = acceptClient(serverSocket);
-
-                SocketHandler socketHandler = new SocketHandler(clientSocket);
-                String username = socketHandler.receive();
-                clients.put(socketHandler, username);
-
-                runChatThreadForClient(socketHandler);
+        Thread UDPServerThread = new Thread(()-> {
+            try (DatagramSocket datagramSocket = new DatagramSocket(SERVER_PORT)) {
+                byte[] receiveBuffer = new byte[1024];
+                Arrays.fill(receiveBuffer, (byte)0);
+                DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+                datagramSocket.receive(receivePacket);
+                String msg = new String(receivePacket.getData());
+                System.out.println("received msg: " + msg);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        });
+
+        Thread TCPServerThread = new Thread(() -> {
+            try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
+                System.out.println("TCP server is listening on port: " + SERVER_PORT);
+                while (true) {
+                    Socket clientSocket = acceptClient(serverSocket);
+                    TCPSocketHandler TCPSocketHandler = new TCPSocketHandler(clientSocket);
+                    String username = TCPSocketHandler.receive();
+                    clients.put(TCPSocketHandler, username);
+                    runChatThreadForClient(TCPSocketHandler);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        TCPServerThread.start();
+        UDPServerThread.start();
     }
 
-    private static void runChatThreadForClient(SocketHandler socketHandler) {
-        Thread chatThread = new ChatThread(socketHandler, clients);
-        clientsThreads.put(socketHandler, chatThread);
+
+    private static void runChatThreadForClient(TCPSocketHandler TCPSocketHandler) {
+        Thread chatThread = new ChatThread(TCPSocketHandler, clients);
+        clientsThreads.put(TCPSocketHandler, chatThread);
         chatThread.start();
     }
 

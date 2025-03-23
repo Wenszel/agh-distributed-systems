7177e377152c7616ff53c51fce5c3d68c84b3291
Wenszel <wiktor.smaga13@gmail.com>
2025-03-23 19:02:03 +0100

    feat: currency and market data uris

--- LAB02/distributed.py
+++ LAB02/distributed.py
@@ -5,16 +5,21 @@ import json
 
 app=FastAPI( )
 
-@app.get("/")
-async def root() :
-    with open('iwda.json', 'r') as file:
-        data = json.load(file)
-    # x = requests.get(uri)
-    # print(x)
+def get_prices_uri(ticker: str, key: str) -> str:
+    return f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&apikey={key}&outputsize=full'
+
+def get_currency_uri(date_YYYY_MM_DD: str, key: str) -> str:
+    return f'https://api.currencyapi.com/v3/historical?apikey={key}&currencies=EUR%2CUSD%2CGBP%2CCHF&base_currency=PLN&date={date_YYYY_MM_DD}'
+
+@app.get("/roi")
+async def root(ticker, currency, date) :
+    uri = get_prices_uri(ticker, ALPHAVANTAGE_KEY)
+    response = requests.get(uri)
+    data = response.json()
     return {"message" : data}
 
 from fastapi.responses import HTMLResponse
-@app.get("/home", response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse)
 async def home() :
     with open('main.html', 'r') as file:
         html = file.read()

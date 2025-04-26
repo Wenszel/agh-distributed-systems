9f1be1cd6bf47a9fe8ec9e2cf1bdb13b5f09ee4a
Wenszel <wiktor.smaga13@gmail.com>
2025-04-26 21:25:23 +0200

    feat: added client template

---
+++ grpc/client/client.js
@@ -0,0 +1,32 @@
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+
+const PROTO_PATH = './subscription.proto';
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH);
+const subscriptionProto = grpc.loadPackageDefinition(packageDefinition).subscription;
+
+const client = new subscriptionProto.EventService('localhost:50051', grpc.credentials.createInsecure());
+
+const request = {
+    cities: ['Warsaw'],
+    eventTypes: [subscriptionProto.EventType.type.value.number]
+};
+
+const call = client.subscribe(request);
+
+call.on('data', notification => {
+    console.log('Received event:', notification);
+});
+
+call.on('end', () => {
+    console.log('Server ended call');
+});
+
+call.on('error', err => {
+    console.error('Error:', err);
+});
+
+call.on('status', status => {
+    console.log('Status:', status);
+});
